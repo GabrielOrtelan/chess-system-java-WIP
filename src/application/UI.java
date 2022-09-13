4327d24914a6097ca73e284d9f8d5ee320aeee48
GabrielOrtelan <gabriel.montanari@bandtec.com.br>
2022-09-13 14:47:33 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -19,7 +19,7 @@ public class UI {
         if(piece == null){
             System.out.print("-");
         } else {
-            System.out.println(piece);
+            System.out.print(piece);
         }
         System.out.print(" ");
     }
